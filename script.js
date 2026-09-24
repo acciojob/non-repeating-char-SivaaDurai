a2f16b30175ebb493844313348f2160a3602c8c2
SivaaDurai <sivasankari0864@gmail.com>
2026-09-24 20:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freq={};
+    let count=0;
+	for(let i=0;i<str.length;i++)
+		{
+			if(freq[str.charAt(i)]===undefined)
+			{
+				freq[str.charAt(i)]=1;
+			}
+			else
+			{
+				freq[str.charAt(i)]++;
+			}
+		}
+	for(let i=0;i<str.length;i++) {
+		if(freq[str.charAt(i)]==1)
+		{
+			console.log(str.charAt(i));
+		}
+		else
+		{
+			console.log("null");
+		}
+	}
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
